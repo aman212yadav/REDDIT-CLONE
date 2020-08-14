a4bf46759257d1ca3b14e89bcc5c34fe587cedf1
aman212yadav <171210007@nitdelhi.ac.in>
2020-08-14 12:38:00 +0530

    user can submit and save the posts

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express=require('express')
 const app=express();
 
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 app.set('view engine','ejs');
+require('./controllers/posts.js')(app);
+require('./data/reddit-db');
+
+
 
 
 app.get("/",(req,res)=>{
      res.render('index');
 });
 
+
+app.get("/posts/new",(req,res)=>{
+  res.render('posts-new');
+});
 
 
 
