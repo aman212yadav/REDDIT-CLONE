7a4ac91042db94940b5066c4ab4c8867f0a7a9bf
aman212yadav <171210007@nitdelhi.ac.in>
2020-08-17 12:06:40 +0530

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,6 +24,38 @@ module.exports = (app) => {
       return res.status(400).send({ err: err });
       });
   });
+  app.get('/login',(req,res)=>{
+      res.render('sign-in');
+  });
+  app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ username }, "username password")
+    .then(user => {
+      if (!user) {
+
+        return res.status(401).send({ message: "Wrong Username or Password" });
+      }
+
+      user.comparePassword(password, (err, isMatch) => {
+        if (!isMatch) {
+          // Password does not match
+          return res.status(401).send({ message: "Wrong Username or password" });
+        }
+
+        const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+          expiresIn: "60 days"
+        });
+        // Set a cookie and redirect to root
+        res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+        res.redirect("/");
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  });
   app.get('/logout', (req, res) => {
    res.clearCookie('nToken');
    res.redirect('/');
